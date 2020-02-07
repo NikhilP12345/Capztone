8390fbf18022c2d90969efbb191a84e410c4b3e8
NikhilP12345 <pariharnikhil92@gmail.com>
2020-02-07 20:13:41 +0530

    update

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
+var admin = require('firebase-admin');
+var serviceAccount = require("./capztone-214f8-firebase-adminsdk-hjiug-fe9e84fb2f.json")
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+app.get("/about", function(req, res) {
+    res.render("about")
+})
+app.get("/staff", (req, res) => {
+    res.render("staff")
+})
+app.get("/solution", (req, res) => {
+    res.render("solution")
+})
+app.get("/train", function(req, res) {
+    res.render("train")
+})
+app.get("/contact", function(req, res) {
+    res.render("contact")
+})
+app.post("/contact", function(req, res) {
+    const body = req.body;
+    let addDoc = db.collection('Record').add(body).then(ref => {
+        res.redirect("/");
+    });
+})
+app.get("/kid", function(req, res) {
+    res.render("kid")
+})
+app.get("/train/data", function(req, res) {
+    res.render("data")
+})
+app.get("/train/bi", function(req, res) {
+    res.render("bi")
+})
+app.listen(3100, process.env.IP, function() {
+    console.log("Product started")
+})
